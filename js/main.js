bb4f77d7023e726ee1ceaa3e3707206b2a6e9a8a
Divya <divyamanoj92@gmail.com>
2014-12-03 13:34:37 +0530

    trips view and main for facnc

--- js/main.js
+++ js/main.js
@@ -1631,6 +1631,7 @@ var r = confirm("Please Select Vehicle Model To Complete The Trip..Click OK to C
 
 
 $('.voucher').on('click',function(){
+	var new_voucher = $(this).attr('new_voucher');
 	var trip_id=$(this).attr('trip_id');
 	var driver_id=$(this).attr('driver_id');
 	var tarrif_id=$(this).attr('tarrif_id');
@@ -1668,6 +1669,7 @@ $('.voucher').on('click',function(){
 	//$('.modal-body').css('position','fixed');
 	$('.trip-voucher-save').attr('trip_id',trip_id);
 	$('.trip-voucher-save').attr('driver_id',driver_id);
+	$('.trip-voucher-save').attr('new_voucher',new_voucher);
 
 
 
@@ -2406,9 +2408,12 @@ function getTariff(minimum_kilometers,rate,additional_kilometer_rate)
 }
 
 $('.trip-voucher-save').on('click',function(){
+	var new_voucher=$(this).attr('new_voucher');
 	var tax_group = $('.taxgroup').val();//tax calculating factor
     var error = false;
-   
+    if(new_voucher == 1 && tax_group == ''){
+	 error = true;
+	}
     var trip_id=$(this).attr('trip_id');
     var driver_id=$(this).attr('driver_id');
 
