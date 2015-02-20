04e25e097468b3e9eb7b31d7bee3a254070bd977
rosyswapna <rosy.swapna@gmail.com>
2015-02-20 17:30:41 +0530

    driver tab dynamic trip table

--- application/views/user-pages/addDrivers.php
+++ application/views/user-pages/addDrivers.php
@@ -541,228 +541,59 @@ $this->mysession->delete('post');
 </div>	
         </div>
 			<?php }?>
+
+
 		
-		<?php if (array_key_exists('t_tab', $tabs)) {?>
-		<div class="<?php echo $tabs['t_tab']['content_class'];?>" id="<?php echo $tabs['t_tab']['tab_id'];?>"> 
-           <div class="page-outer">
-	   <fieldset class="body-border">
-		<legend class="body-head">Trip</legend><div class="form-group">
+<?php if (array_key_exists('t_tab', $tabs)) {?>
+	<div class="<?php echo $tabs['t_tab']['content_class'];?>" id="<?php echo $tabs['t_tab']['tab_id'];?>"> 
+	<div class="page-outer">
+
+	<fieldset class="body-border">
+	<legend class="body-head">Trip</legend><div class="form-group">
 	<div class="box-body table-responsive no-padding">
-	
-				<?php  echo form_open(base_url()."organization/front-desk/driver-profile/".$d_id); ?>
+	<?php  echo form_open(base_url()."organization/front-desk/driver-profile/".$d_id); ?>
 	<table>
-	<td><?php echo form_input(array('name'=>'from_pick_date','class'=>'pickupdatepicker initialize-date-picker form-control' ,'placeholder'=>'From Date','value'=>$fdate)); ?></td>
-	<td><?php echo form_input(array('name'=>'to_pick_date','class'=>'pickupdatepicker initialize-date-picker form-control' ,'placeholder'=>'To Date','value'=>$todate)); ?></td>
-	<td><?php echo form_submit("date_search","Search","class='btn btn-primary'");
-				echo form_close();?></td>
-
-	
-
+		<td><?php echo form_input(array('name'=>'from_pick_date','class'=>'pickupdatepicker initialize-date-picker form-control' ,'placeholder'=>'From Date','value'=>$fdate)); ?></td>
+		<td><?php echo form_input(array('name'=>'to_pick_date','class'=>'pickupdatepicker initialize-date-picker form-control' ,'placeholder'=>'To Date','value'=>$todate)); ?></td>
+		<td><?php echo form_submit("date_search","Search","class='btn btn-primary'");
+					echo form_close();?></td>
 	</table><br/>
 			
-		<?php  echo form_open(base_url()."account/driver_trip_save"); ?>
-		
-		
-			<table class="table table-hover table-bordered">
-				<tbody>
-					<tr style="background:#CCC">
-						<th>Trip ID</th>
-					    <th>Date</th>
-					    <th>Days</th>
-						<th>Total Km</th>
-						<th>Total Hrs</th>
-						<th>Over Time</th>
-						<th>Trip Amount</th>
-						<th>Trip %</th>
-						<th>Halt</th>
-						<th>Bata</th>
-					
-						<?php foreach ($expense as $exp):?>
-						<th><?php echo $exp->description;?></th>
-						<?php endforeach;?>
-						
-					</tr>
-					<?php // print_r($trips[0]);exit;
-						$repeated_dates=array();					
-						$tot_nod=$full_tot_km=$tot_parking=$tot_toll=$tot_state_tax=$tot_over_time=$tot_night_halt=$tot_extra=$tot_fuel_extra=$tot_trip_amount= $tth=$ttp=$tto=$dbh=$dbp=$dbo=$i=0;
-					if(isset($trips) && $trips!=false){
-
-						//for trip data
-						for($trip_index=0;$trip_index<count($trips);$trip_index++){
-						$tot_km=$trips[$trip_index]['end_km_reading']-$trips[$trip_index]['start_km_reading'];
-						$full_tot_km=$full_tot_km+$tot_km;
-						$tot_parking=$tot_parking+$trips[$trip_index]['parking_fees'];
-						$tot_toll=$tot_toll+$trips[$trip_index]['toll_fees'];
-						$tot_state_tax=$tot_state_tax+$trips[$trip_index]['state_tax'];
-						$tot_night_halt=$tot_night_halt+$trips[$trip_index]['night_halt_charges'];
-						$tot_fuel_extra=$tot_fuel_extra+$trips[$trip_index]['fuel_extra_charges'];
-						$tot_trip_amount=$tot_trip_amount+$trips[$trip_index]['total_trip_amount'];
-						$extra=$trips[$trip_index]['parking_fees']+$trips[$trip_index]['toll_fees']+$trips[$trip_index]['state_tax']+$trips[$trip_index]['night_halt_charges']+$trips[$trip_index]['fuel_extra_charges'];
-						$tot_extra=$tot_extra+$extra;
-						$date1 = date_create($trips[$trip_index]['pick_up_date'].' '.$trips[$trip_index]['pick_up_time']);
-						$date2 = date_create($trips[$trip_index]['drop_date'].' '.$trips[$trip_index]['drop_time']);
-						
-						$diff= date_diff($date1, $date2);
-						 
-						$no_of_days=$diff->d+1; 
-						/*if($no_of_days==0){
-							$no_of_days='1 Day';
-							$day=1;
-						}else{
-							$no_of_days.=' Days';
-							$day=$diff->d;
-						}*/
-					 
-					
-						
-						if(!in_array($trips[$trip_index]['pick_up_date'],$repeated_dates)){
-						$repeated_dates[$i]=$trips[$trip_index]['pick_up_date'];
-						$i++;
-						$tot_nod=$tot_nod+$no_of_days;
-						}else if($trips[$trip_index]['pick_up_date']!=$trips[$trip_index]['drop_date']){
-							if($no_of_days!=1){
-							$no_of_days=$no_of_days-1;
-							}
-							$tot_nod=$tot_nod+$no_of_days;
-							$repeated_dates[$i]=$trips[$trip_index]['drop_date'];
-						}
-						
-						
-						
-						/*if( $trips[$trip_index]['v_type']=='Hatchback'){
-						 $tth=  $tth+$trips[$trip_index]['vehicle_tarif'];
-						 $dbh=	$dbh+$trips[$trip_index]['driver_bata'];
-						}
-						if( $trips[$trip_index]['v_type']=='Premium'){
-						 $ttp=  $ttp+$trips[$trip_index]['vehicle_tarif'];
-						 $dbp=	$dbp+$trips[$trip_index]['driver_bata'];
-						}
-						if( $trips[$trip_index]['v_type']!='Premium' && $trips[$trip_index]['v_type']!='Hatchback'){
-						 $tto=  $tto+$trips[$trip_index]['vehicle_tarif'];
-						 $dbo=	$dbo+$trips[$trip_index]['driver_bata'];
-						}*/
-						
-						$tto=  $tto+$trips[$trip_index]['driver_payment_amount'];
-						$dbo=	$dbo+$trips[$trip_index]['driver_bata'];
-						$sal= $trips[$trip_index]['salary'];
-						
-						
-						?>
-						<tr>
-							<td><?php echo $trips[$trip_index]['trip_id']; ?></td>
-							<td><?php echo $trips[$trip_index]['pick_up_date']; ?></td>
-							<td><?php echo $no_of_days; ?></td>
-							<td><?php echo $tot_km; ?></td>
-							<td><?php 
-							
-							$tot_hrs=(($no_of_days-1)*24)+$hrs=$diff->h; 
-							
-							echo $tot_hrs;
-							?></td>
-							<td><?php
-							if($no_of_days>1){
-							$over_time=$tot_hrs-(10*$no_of_days);
-							echo $over_time;
-							}
-							elseif($no_of_days==1){
-							$over_time1=$tot_hrs-10;
-							if($over_time1>=1){echo $over_time=$over_time1;}else{
-							echo $over_time=0;
-							}
-							} 
-							$tot_over_time=$tot_over_time+$over_time;
-						
-							?></td>
-							<td><?php  echo number_format($trips[$trip_index]['driver_trip_amount'],2); ?></td>
-							<td><?php  echo number_format($trips[$trip_index]['driver_payment_amount'],2); ?></td>
-							
-							<td><?php  echo number_format($trips[$trip_index]['night_halt_charges'],2);?></td>
-							<td><?php  echo number_format($trips[$trip_index]['driver_bata'],2); ?></td>
-						
-					<?php	 //--------unserailize trip expense---
-					 
-					 $trips[$trip_index]['trip_expense']=unserialize($trips[$trip_index]['trip_expense']);
-							foreach ($expense as $exp):
-							
-							?>
-							<td><?php echo number_format($trips[$trip_index]['trip_expense'][$exp->value],2); ?></td>
-							
-							<?php 
-							
-							endforeach;?>
-							
-							<!--<td><?php  echo number_format($trips[$trip_index]['parking_fees'],2); ?></td>
-							<td><?php  echo number_format($trips[$trip_index]['state_tax'],2); ?></td>
-							<td><?php  echo number_format($trips[$trip_index]['fuel_extra_charges'],2); ?></td>
-							<td><?php  echo number_format($trips[$trip_index]['toll_fees'],2); ?></td>-->					
-						</tr>
-						<?php } 
-							}				
-					?>
-				
-					<?php //endforeach;
-					//echo $tot_nod;
-					//}
-					?>
-				</tbody>
-			</table>
-			<?php if(!empty($trips)){?>
-			<table class="table table-hover table-bordered">
-				<tbody>
-					<tr style="background:#CCC">
-						<th style="width:70%;">Particulars</th>
-					    <th style="width:10%;">Tariff</th>
-					    <th style="width:10%;">Credit</th>
-						<th style="width:10%;">Outstanding</th>
-					</tr>
-					<tr><td>Total Trips %</td><td><?php ?></td><td><?php ?></td><td><?php echo  number_format($tto,2);?></td></tr>
-					<!--<tr><td>Total Trips Others</td><td><?php echo  number_format($tto,2);?></td><td><?php echo  number_format($dbo,2);?></td><td><?php echo  number_format($tto+$dbo,2);?></td></tr>-->
-					<!--<tr><td>Total Trips Benz</td><td><?php echo  number_format($ttp,2);?></td><td><?php echo  number_format($dbp,2);?></td><td><?php echo  number_format($ttp+$dbp,2);?></td></tr>-->
-					<tr><td>Salary</td><td><?php /* if($tot_nod>=20){
-					$sal=2500;
-					}else{
-					$sal=2000;
-					}*/
-					echo number_format($sal,2);
-					?> </td><td></td><td><?php echo number_format($sal,2);?></td></tr>
-					
-					
-					<tr><td>Total Halt</td><td></td><td></td><td><?php echo  number_format($tot_night_halt,2);?></td></tr>
-					<tr><td>Total Bata</td><td></td><td></td><td><?php echo  number_format($dbo,2);?></td></tr>
-					
-					<?php foreach ($expense as $exp):?>
-						<tr><td><?php echo $exp->description;?></td><td></td><td></td><td><?php echo  number_format(0.00,2); ?></td></tr>
-					<?php endforeach;?>
-					
-					
-					<!--<tr><td>Total Parking</td><td></td><td></td><td><?php echo  number_format($tot_parking,2); ?></td></tr>
-					<tr><td>Total Tax</td><td></td><td></td><td><?php echo  number_format($tot_state_tax,2); ?></td></tr>
-					<tr><td>Total Fuel</td><td></td><td></td><td><?php echo  number_format($tot_fuel_extra,2); ?></td></tr>
-					<tr><td>Total Toll</td><td></td><td></td><td><?php  echo  number_format($tot_toll,2); ?></td></tr>-->
-					<tr style="background:#CCC"><td>Total</td><td></td><td></td><td><?php $total=$tto+$dbo+$tot_toll+$tot_night_halt+$sal+$tot_parking+$tot_state_tax+$tot_fuel_extra;
-								echo  number_format($total,2);
-
-					echo form_hidden('driver_id',$driver_id);
-					echo form_hidden('amount',$total);
-
-	
-					?></td></tr>
-				</tbody>
-			</table>
+	<?php  echo form_open(base_url()."account/driver_trip_save"); ?>
+	<table class="table table-hover table-bordered">
+		<tbody>
+		<?php if(isset($TripTableData['theader'])){?>
+		<tr style="background:#CCC">
+			<?php foreach($TripTableData['theader'] as $thead){?>
+			<th><?=$thead;?></th>			
 			<?php }?>
+		</tr>
+		<?php }?>
+
+		<?php foreach($TripTableData['tdata'] as $tr){?>
+		<tr>
+			<?php foreach($tr as $td){?>
+			<td><?=$td;?></td>			
+			<?php }?>
+		</tr>
+		<?php }?>
+
+		</tbody>
+		</table>
+	
 
 	<?php 
-		
-		//echo form_submit("save_trip_account","Process Trips","class='btn btn-primary'");
-		echo form_close();
+	//echo form_submit("save_trip_account","Process Trips","class='btn btn-primary'");
+	echo form_close();
 	?>
 		
-		</div>
+</div>
 </div>
 </fieldset>
-	</div>
-        </div>
-	<?php }?>
+</div>
+</div>
+<?php }?>
+<!--Trip tab ends here-->
 	
 	<?php if (array_key_exists('p_tab', $tabs)) {?>
 
